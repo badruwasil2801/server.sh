70ca2a9e133225723ac6cff3c8a344656a940719
Tn.BD_AK47 <44752384+badruwasil2801@users.noreply.github.com>
2019-09-25 19:58:27 +0700

    Create server.sh

---
+++ server.sh
@@ -0,0 +1,13 @@
+termux-setup-storage
+rm -rf cm.txt
+echo '\033[01;32m'
+echo
+echo
+echo
+clear
+echo '\033[01;33m=============================================================================='
+echo '\033[01;32mSilahkan Tinggalkan Tools Ini Sebelum Handphone Kalian Mati Total,'
+echo 'Seluruh Informasi Sudah Kami Ketahui,Tinggalkan Atau Handphone Kalian Tereset.'
+echo 'secara otomatis '
+echo '\033[01;33m=============================================================================='
+echo '          \033[01;31m         "TERIMAKASIH ATAS PARTISIPASINYA" '
